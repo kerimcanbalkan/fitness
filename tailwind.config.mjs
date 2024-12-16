3d6cf4d880da78814329e9da7ea54d1db2218f9a
Kerimcan Balkan <kerimcanbalkan@gmail.com>
2024-12-16 20:04:30 +0300

    navbar and hero complete

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'hero': "url('./src/assets/hero.jpg')"
+			},
+			colors: {
+				'white': "#d5ccba",
+				'cyan': "#989a9c",
+				'magenta': "#97522c",
+				'blue': "#426a79",
+				'yellow': "#eaa549",
+				'green': "#858162",
+				'red': "#be100e",
+				'black': "#5e5252",
+			}
+		},
+	},
+	plugins: [],
+}
