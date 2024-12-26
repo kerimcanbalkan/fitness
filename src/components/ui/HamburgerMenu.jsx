9f30091441fb18e028bb7ae140ff9b7c8dc22e77
Kerimcan Balkan <kerimcanbalkan@gmail.com>
2024-12-26 16:43:24 +0300

    Removed user part

--- src/components/ui/HamburgerMenu.jsx
+++ src/components/ui/HamburgerMenu.jsx
@@ -13,32 +13,31 @@ export default function HamburgerMenu() {
 			>
 				<div
 					className={`${genericHamburgerLine} ${isOpen
-						? "rotate-45 translate-y-1.5 opacity-100"
-						: "opacity-100"
-						}`}
+? "rotate-45 translate-y-1.5 opacity-100"
+: "opacity-100"
+}`}
 				/>
 				<div
 					className={`${genericHamburgerLine} ${isOpen ? "opacity-0" : "opacity-100"
-						}`}
+}`}
 				/>
 				<div
 					className={`${genericHamburgerLine} ${isOpen
-						? "-rotate-45 -translate-y-1.5 opacity-100"
-						: "opacity-100"
-						}`}
+? "-rotate-45 -translate-y-1.5 opacity-100"
+: "opacity-100"
+}`}
 				/>
 			</button>
 
 			<div
 				className={`fixed w-screen h-screen bg-[#fff] text-3xl top-0 left-0 flex flex-col justify-center items-center z-40
-					transition-opacity duration-500 ease-in-out
-					${isOpen ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'} `}
+transition-opacity duration-500 ease-in-out
+${isOpen ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'} `}
 				onClick={() => setIsOpen(!isOpen)}
 			>
 				<a href="/" className="text-black my-2 hover:text-magenta">Home</a>
 				<a href="#About" className="text-black my-2 hover:text-magenta">About</a>
 				<a href="#Contact" className="text-black my-2 hover:text-magenta">Contact</a>
-				<a href="#User" className="text-black my-2 hover:text-magenta">User</a>
 			</div>		</div>
 	);
 }
