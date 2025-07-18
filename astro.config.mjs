73459ca9269f4e4106db4956e58ad57a0060da4b
Kerimcan Balkan <kerimcanbalkan@protonmail.com>
2025-07-18 20:06:07 +0300

    gh pages debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()]
+  integrations: [tailwind(), icon(), react()],
+  output: "static",
 });
